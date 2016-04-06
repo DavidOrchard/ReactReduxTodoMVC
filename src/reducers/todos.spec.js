3e06061475a1ad8bc0ec2ace875f9395ee18f72a
David Orchard <dorchard@walmartlabs.com>
2016-04-06 07:48:54 -0700

    add todo test

--- src/reducers/todos.spec.js
+++ src/reducers/todos.spec.js
@@ -19,4 +19,14 @@ describe('App', () => {
    it('should have a default state', function () {
     expect(todos(undefined, NULL_ACTION)).to.not.eql(DEFAULT_STATE);
   });
+
+  describe('add todo', function() {
+    const stateBefore = [];
+    const action = {type: 'ADD_TODO', id:0, text: 'Learn Redux'};
+    const stateAfter = [{id:action.id, text:action.text, completed:false}];
+    it('should add todo', function() {
+      expect(todos(stateBefore, action)).to.eql(stateAfter);
+    });
+
+  });
 });
