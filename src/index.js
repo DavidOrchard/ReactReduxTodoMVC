1c3e796ba6bd415939b18714a764cc4130fe80c7
David Orchard <dorchard@walmartlabs.com>
2016-04-07 17:04:54 -0700

    add working default css

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import configureStore from './store/configureStore';
-import './styles/styles.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-
+import '../node_modules/todomvc-common/base.css'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import '../node_modules/todomvc-app-css/index.css';
 const store = configureStore();
 
 render(
